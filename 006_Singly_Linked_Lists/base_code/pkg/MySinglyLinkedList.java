3808b77e9baacffe8070a5a297a7980b288cc20f
JacobPoole <jacob.poole589@gmail.com>
2025-08-24 20:33:14 -0700

    Upload

---
+++ 006_Singly_Linked_Lists/base_code/pkg/MySinglyLinkedList.java
@@ -0,0 +1,45 @@
+package pkg;
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class MySinglyLinkedList {
+	Node head;
+
+	/* 
+		Postcondition: The head will be null 
+	*/
+	public MySinglyLinkedList() {
+
+	}
+
+	/* 
+		Receives an integer position, searches through the SinglyLinkedList for the position and returns the data at that positon
+	   	If the position doesn't exist, it returns -1
+	*/ 
+	public int get(int pos){
+
+	}
+
+	/*
+		Insert a new Node at the given position with the data given
+	*/
+	public void insert(int pos int data){
+
+	}
+
+	/*
+		Remove the node at the given position
+		If no position exists, don't change the list
+	*/
+	public void remove(int pos){
+
+	}
+
+	/*
+		Print all data values in the LinkedList 
+	*/
+	public void printList(){
+
+	}
+}
